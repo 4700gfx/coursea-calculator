713dc05d2823be38d08a3722e70f17218643274a
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-05-19 13:03:56 -0400

    Adding Event Handlers on Input

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Buttons from './components/Buttons'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState(0)
+  const [input, setInput] = useState('Enter A Number') //
+
+  const handleInput = (event) => {
+    setInput(Number(event.target.value))
+    console.log(input)
+  }
 
 
   return (
     <div className='flex flex-col justify-center items-center'>
       <h1 className='text-4xl font-bold mb-5'>Simple Calculator App</h1>
-      <span className='mb-4'>Total:</span>
-      <input type='text' className='rounded-full px-10 py-1 bg-gray-400 my-5' placeholder='Try A Number'></input>
+      <h2 className='text-2xl'>Total:</h2>
+      <span className='mb-4 text-7xl'>{input}</span>
+      <input type='text' onChange={handleInput} className='number-input rounded-full px-10 py-1 bg-gray-400 my-5' placeholder={input}></input>
       <Buttons></Buttons>
       
     </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 
-const Buttons = () => {
-  const buttonChecker = () =>{
-    const button = document.querySelector('.operation-button')
-    console.log(button.textContent)
+const Buttons = (props) => {
+
+  const handleAddition = () =>{
+    console.log(props.total)
   }
-  
-  
-  
+
+    
   return (
       <div className='button-row'>
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Add ➕</button>
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Subtract ➖</button>
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Multiply ✖</button>
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Divide ➗</button>
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Reset Input 🔢</button>       
-        <button onClick={buttonChecker} className='operation-button bg-blue-500 text-white p-4 me-4 rounded-3xl'>Reset Result 🧮</button>
+        <button id='operation-add' onClick={handleAddition} className='bg-blue-500 text-white p-4 me-4 rounded-3xl'>+</button>
+        <button id='operation-subtract' className='bg-blue-500 text-white p-4 me-4 rounded-3xl'> - </button>
+        <button id='operation-multiply' className='bg-blue-500 text-white p-4 me-4 rounded-3xl'> * </button>
+        <button id='operation-divide' className='bg-blue-500 text-white p-4 me-4 rounded-3xl'> / </button>
+        <button id='operation-input' className='bg-blue-500 text-white p-4 me-4 rounded-3xl'>Reset Input</button>       
+        <button id='operation-result' className='bg-blue-500 text-white p-4 me-4 rounded-3xl'>Reset Result</button>
       </div>
   )
 }
